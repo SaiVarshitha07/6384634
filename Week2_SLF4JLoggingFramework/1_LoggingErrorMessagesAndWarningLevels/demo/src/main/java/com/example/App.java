839f8ede5c9dcdb514ef9c4fc965d21520d195b5
Sai Varshitha <saivarshithasakaray@gmail.com>
2025-06-30 08:49:49 +0530

    commit

--- Week2_SLF4JLoggingFramework/1_LoggingErrorMessagesAndWarningLevels/demo/src/main/java/com/example/App.java
+++ Week2_SLF4JLoggingFramework/1_LoggingErrorMessagesAndWarningLevels/demo/src/main/java/com/example/App.java
@@ -1,13 +1,21 @@
-package com.example;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Application started");
+
+        int age = 16;
+        if (age < 18) {
+            logger.warning("Warning: Underage user detected!");
+        }
+
+        try {
+            int result = 10 / 0;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error: Division by zero", e);
+        }
     }
 }
